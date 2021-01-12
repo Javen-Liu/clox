3e897127e27293ebead1f62380c8dd83db1e6bb7
Javen-Liu <1248540980@qq.com>
2021-01-12 23:52:18 +0800

    added rainbow print, which can print the message in rainbow color. normal message will print with rainbow color; warning message will print with yellow color; error message will print with red color;

---
+++ rainbow.c
@@ -0,0 +1,44 @@
+//
+// Created by Javen on 2021/1/12.
+//
+
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "rainbow.h"
+
+#define F 0.1
+#define PI 3.14
+#define RED(i)      sin(F*(float)(i)+0) * 127 + 128
+#define GREEN(i)    sin(F*(float)(i)+2*PI/3) * 127 + 128
+#define BLUE(i)     sin(F*(float)(i)+4*PI/3) * 127 + 128
+
+void rainbow(const char *message){
+    int len = (int) strlen(message);
+    for(int i = 0; i < len; i++){
+        int r = RED(i), g = GREEN(i), b = BLUE(i);
+        printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, message[i]);
+    }
+}
+
+void warning(const char *message){
+    int len = (int) strlen(message);
+    for(int i = 0; i < len; i++){
+        printf("\033[38;2;%d;%d;%dm%c\033[0m", 255, 215, 0, message[i]);
+    }
+}
+
+//243,132,130
+void error(const char *message){
+    int len = (int) strlen(message);
+    for(int i = 0; i < len; i++){
+        printf("\033[38;2;%d;%d;%dm%c\033[0m", 243, 132, 130, message[i]);
+    }
+}
+
+void test(){
+    rainbow("hello world, this is the print of rainbow!");
+    warning("hello world, this is the warning print");
+    error("hello world, this is the error print");
+}
---
+++ rainbow.h
@@ -0,0 +1,16 @@
+//
+// Created by Javen on 2021/1/12.
+//
+
+#ifndef CLOX_RAINBOW_H
+#define CLOX_RAINBOW_H
+
+void rainbow(const char *message);
+void warning(const char *message);
+void error(const char *message);
+
+char *rainbows(const char *message);
+char *warnings(const char *message);
+char *errors(const char *message);
+
+#endif //CLOX_RAINBOW_H
