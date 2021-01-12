931d47dd332dd0184106ade0ad15ea7affbfcde4
Javen-Liu <1248540980@qq.com>
2021-01-12 23:59:45 +0800

    Add compiler and scanner. Scanner scans the source which can be read from file or terminal. Compiler compile the tokens, which are scanned by scanner, to the bytecode chunk. And then the interpreter will interpret the chunk code. Compiler is half way of completion. And this version change the interpret() function, the parameters become the source, which we read from file. That means the process of interpreting contains the compiling.

---
+++ compiler.h
@@ -0,0 +1,10 @@
+//
+// Created by Javen on 2021/1/12.
+//
+
+#ifndef CLOX_COMPILER_H
+#define CLOX_COMPILER_H
+
+void compile(const char *source);
+
+#endif //CLOX_COMPILER_H
--- main.c
+++ main.c
@@ -1,47 +1,87 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
 #include "vm.h"
+#include "rainbow.h"
+
+static void repl() {
+    char line[1024];
+    for(;;) {
+        rainbow("> ");
+
+        if(!fgets(line, sizeof(line), stdin)) {
+            printf("\n");
+            break;
+        }
+
+        interpret(line);
+    }
+}
+
+static char *readFile(const char *path) {
+    FILE *file = fopen(path, "rb");
+    if(file == NULL) {
+        // todo 将rainbow组建改为拼接字符串
+        error("Could not open file \"");
+        error(path);
+        error("\".\n");
+        exit(74);
+    }
+
+    fseek(file, 0L, SEEK_END);
+    size_t fileSize = ftell(file);
+    rewind(file);
+
+    char *buffer = (char *)malloc(fileSize + 1);
+    if(buffer == NULL) {
+        // todo 将rainbow组建改为拼接字符串
+        error("Not enough memory to read \"");
+        error(path);
+        error("\".\n");
+        exit(74);
+    }
+
+    size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+    if(bytesRead < fileSize) {
+        // todo 将rainbow组建改为拼接字符串
+        error("Could not open file \"");
+        error(path);
+        error("\".\n");
+        exit(74);
+    }
+
+    buffer[bytesRead] = '\0';
+
+    fclose(file);
+    return buffer;
+}
+
+static void runFile(const char *path) {
+    char *source = readFile(path);
+    InterpretResult result = interpret(source);
+    free(source);
+
+    if(result == INTERPRET_COMPILE_ERROR) exit(65);
+    if(result == INTERPRET_RUNTIME_ERROR) exit(70);
+}
 
 int main(int argc, const char* argv[]) {
     initVM();
 
-	Chunk chunk;
-    initChunk(&chunk);
-
-    // 写入常数1.2
-    int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
-    constant = addConstant(&chunk, 3.4);
-    writeChunk(&chunk, OP_CONSTANT, 124);
-    writeChunk(&chunk, constant, 124);
-
-    writeChunk(&chunk, OP_ADD, 124);
-
-    constant = addConstant(&chunk, 5.6);
-    writeChunk(&chunk, OP_CONSTANT, 124);
-    writeChunk(&chunk, constant, 124);
-
-    writeChunk(&chunk, OP_DIVIDE, 124);
-
-    // 写入负号 "-"
-    writeChunk(&chunk, OP_NEGATE, 125);
-
-    // 写入返回操作符
-    writeChunk(&chunk, OP_RETURN, 125);
-
-    // 解析chunk里面的内容
-    disassembleChunk(&chunk, "test chunk");
-    printf("\n");
-
-    // 对chunk字节码里的内容进行解释执行
-    interpret(&chunk);
+    if(argc == 1){
+        repl();
+    } else if (argc == 2) {
+        runFile(argv[1]);
+    } else {
+        rainbow("Usage: clox [path]\n");
+        exit(64);
+    }
 
     freeVM();
-    freeChunk(&chunk);
+    // freeChunk(&chunk);
     return 0;
 }
